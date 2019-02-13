19d59eaa817e8e55108a5437a80fac5f4038918b
Wellyson Martins <wellysonmartins@gmail.com>
2019-02-13 11:30:21 -0200

    importacao fundamentos

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,18 @@
+// Função sem retorno
+function imprimirSoma(a, b) {
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3) // 5
+imprimirSoma(2) // NaN
+imprimirSoma(2, 3, 4, 5, 6) // 5
+imprimirSoma() // NaN
+
+// Função com retorno
+function soma(a, b = 0) {
+    return a + b
+}
+
+console.log(soma(2, 3)) // 5
+console.log(soma(2)) // 2
+console.log(soma()) // NaN
---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,14 @@
+// Infinity
+console.log(7 / 0)
+
+// String
+console.log("10" / 2)
+
+//String NaN
+console.log("Show!" * 2)
+
+//Ponto flutuante
+console.log(0.1 + 0.7)
+
+//Chamar função
+//console.log(10.toString())
