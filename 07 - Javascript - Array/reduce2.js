47bae065aab49705279fed830a8ad767b032346e
Wellyson Martins <wellysonmartins@gmail.com>
2019-03-05 18:27:55 -0300

    Reduce 3

--- 07 - Javascript - Array/reduce2.js
+++ 07 - Javascript - Array/reduce2.js
@@ -6,5 +6,9 @@ const alunos = [
 ]
 
 // Desafio 1: Todos os alunos são bolsista?
+const todosBolsistas = (resultado, bolsita) => resultado && bolsista
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
 
 // Desafio 2: Algum aluno é bolsista?
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
