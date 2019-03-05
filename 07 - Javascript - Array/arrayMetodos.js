98d4ecc333ae3c20733251d7d2c6071364fc2cbf
Wellyson Martins <wellysonmartins@gmail.com>
2019-03-05 00:10:22 -0300

    Foreach

---
+++ 07 - Javascript - Array/arrayMetodos.js
@@ -0,0 +1,28 @@
+const pilotos = ['Vettel', 'Alonso', 'Raikkonen', 'Massa']
+pilotos.pop() // remove o último
+console.log(pilotos)
+
+pilotos.push('Verstappen')
+console.log(pilotos)
+
+pilotos.shift() // remove o primeiro!
+console.log(pilotos)
+
+pilotos.unshift('Hamilton') // adiciona no primeiro
+console.log(pilotos)
+
+// splice pode adicionar e remover elementos
+
+// adicionar
+pilotos.splice(2, 0, 'Bottas', 'Massa')
+console.log(pilotos)
+
+// remover
+pilotos.splice(3, 1)  // massa queboru :(
+console.log(pilotos)
+
+const algunsPilotos1 = pilotos.slice(2) // novo array
+console.log(algunsPilotos1)
+
+const algunsPilotos2 = pilotos.slice(1, 4)
+console.log(algunsPilotos2)
